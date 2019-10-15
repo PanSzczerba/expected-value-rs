1e1155ec7108b9d149b851599c5c4099204f685c
PanSzczerba <26200369+PanSzczerba@users.noreply.github.com>
2019-10-15 18:41:48 +0200

    Added single threaded version

--- src/lib.rs
+++ src/lib.rs
@@ -67,9 +67,8 @@ mod test {
 }
 
 use std::error::Error;
-use std::thread;
 
-use rand;
+extern crate rand;
 use rand::distributions::Uniform;
 use rand::Rng;
 
@@ -99,7 +98,6 @@ fn max_subsequence_sum(v: &[f64]) -> f64 {
 
 #[derive(Debug)]
 pub struct Config {
-    pub thread_num: usize,
     pub samples: usize,
     pub sample_size: usize,
 }
@@ -110,54 +108,21 @@ struct Average {
     elements_count: usize,
 }
 
-impl Average {
-    fn merge(self, other: Average) -> Average {
-        assert!(
-            self.elements_count != 0 || other.elements_count != 0,
-            "Both averages have elements_count equal to zero"
-        );
-        let new_average = (self.average * self.elements_count as f64
-            + other.average * other.elements_count as f64)
-            / (self.elements_count + other.elements_count) as f64;
-        Average {
-            average: new_average,
-            elements_count: self.elements_count + other.elements_count,
-        }
-    }
-}
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut handles = Vec::new();
-
-    for _ in 0..config.thread_num {
-        let samples = config.samples;
-        let sample_size = config.sample_size;
-        handles.push(thread::spawn(move || {
-            let rng = rand::thread_rng();
-            let mut average = Average {
-                average: 0.0,
-                elements_count: 0,
-            };
-            for _ in 0..samples {
-                let sample: Vec<f64> = rng
-                    .sample_iter(Uniform::new_inclusive(-1.0, 1.0))
-                    .take(sample_size)
-                    .collect();
-                let sum = max_subsequence_sum(&sample);
-                average.elements_count += 1;
-                average.average += (sum - average.average) / average.elements_count as f64;
-            }
-            average
-        }));
-    }
-
+    let rng = rand::thread_rng();
     let mut average = Average {
         average: 0.0,
         elements_count: 0,
     };
 
-    for handle in handles {
-        average = average.merge(handle.join().unwrap());
+    for _ in 0..config.samples {
+        let sample: Vec<f64> = rng
+            .sample_iter(Uniform::new_inclusive(-1.0, 1.0))
+            .take(config.sample_size)
+            .collect();
+        let sum = max_subsequence_sum(&sample);
+        average.elements_count += 1;
+        average.average += (sum - average.average) / average.elements_count as f64;
     }
 
     println!("{}", average.average);
--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,6 @@ fn main() {
             .help("Specifies how much samples will be processed in order to calculate their average. Note that it should always be a multiple of used threads")
             .required(true)
             .index(2))
-        .arg(Arg::with_name("threads")
-            .short("t")
-            .long("threads")
-            .value_name("THREAD NUMBER")
-            .help("Number of threads used in counting average. If not set defaults to number of avilable processors"))
         .get_matches();
 
     let sample_size: usize = matches
@@ -42,18 +37,9 @@ fn main() {
             process::exit(1)
         });
 
-    let thread_num: usize = match matches.value_of("threads") {
-        Some(s) => s.parse().unwrap_or_else(|e| {
-            eprintln!("Error: Could not parse thread number: {}", e);
-            process::exit(1)
-        }),
-        None => num_cpus::get(),
-    };
-
     let config = Config {
         sample_size,
-        samples: samples / thread_num,
-        thread_num,
+        samples
     };
 
     if let Err(e) = expected_value::run(config) {
